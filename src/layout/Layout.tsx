26c8dd5a6303f8211d53c3d503536d3414ddf763
renzp94 <1074720760@qq.com>
2022-04-06 23:14:45 +0800

    chore: 添加暗黑模式下烟花效果

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,9 +4,13 @@ const Layout = () => {
   return (
     <section>
       <Header />
-      <main className="px-6 lg:px-20 lg:(w-2/4 mx-auto)">
+      <main className=" px-6 lg:px-20 lg:(w-2/4 mx-auto)">
         <Outlet />
       </main>
+      <div className="fireworks" style={{ left: '10%', top: '10%' }} />
+      <div className="fireworks" style={{ left: '35%', top: '35%', animationDelay: '-0.4s' }} />
+      <div className="fireworks" style={{ left: '60%', top: '23%', animationDelay: '-1.7s' }} />
+      <div className="fireworks" style={{ left: '70%', top: '8%', animationDelay: '-3.1s' }} />
     </section>
   )
 }
